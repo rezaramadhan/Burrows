31d1605225788c3f30a547924cda80e5c81b1eb6
Mayu <mayuuzu@gmail.com>
2017-02-23 20:50:33 +0700

    Membuat layout main

--- Burrows/app/src/main/java/id/wesudgitgud/burrows/MainActivity.java
+++ Burrows/app/src/main/java/id/wesudgitgud/burrows/MainActivity.java
@@ -2,12 +2,35 @@ package id.wesudgitgud.burrows;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ViewFlipper viewFlipper;
+    Button next;
+    Button prev;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
+        next = (Button) findViewById(R.id.btnNext);
+        prev = (Button) findViewById(R.id.btnPrev);
+
+        next.setOnClickListener(this);
+        prev.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view == next){
+            viewFlipper.showNext();
+        } else {
+            viewFlipper.showPrevious();
+        }
     }
 }
