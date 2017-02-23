4e6ebd0c84a2619c5f6b99334c98869e793e9110
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-02-23 09:53:24 +0700

    Login & register success

--- Burrows/app/src/main/java/id/wesudgitgud/burrows/RegisterActivity.java
+++ Burrows/app/src/main/java/id/wesudgitgud/burrows/RegisterActivity.java
@@ -1,22 +1,90 @@
 package id.wesudgitgud.burrows;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import id.wesudgitgud.burrows.models.User;
 
 public class RegisterActivity extends AppCompatActivity {
+    private final String TAG = "RegisterActivity";
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference firebaseDatabase;
+    private DatabaseReference userDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance().getReference();
 
+        userDatabase = firebaseDatabase.child("user");
     }
 
     public void goToLogin(View view){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+    public void signUp (View view) {
+        String email = ((EditText) findViewById(R.id.formEmail)).getText().toString();
+        String password = ((EditText) findViewById(R.id.formPassword)).getText().toString();
+        String username = ((EditText) findViewById(R.id.formUsername)).getText().toString();
+        String fullname = ((EditText) findViewById(R.id.formFullname)).getText().toString();
+
+        Log.d(TAG, "Signing up\n" + email + "\n" + password + "\n" + username + "\n" + fullname);
+        createNewUser(email, password, username, fullname);
+//        addUsertoDB("bagus@gmail.com", "bagus", "Bagus Sekali");
+    }
+
+    private void createNewUser(final String email, String password, final String username, final String fullname) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "SIGNIN:------------------------" + task.isSuccessful());
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG, "-----failed" + task.getException());
+                            Toast.makeText(RegisterActivity.this, task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Signin success",
+                                    Toast.LENGTH_SHORT).show();
+
+                            addUsertoDB(email, username, fullname);
+
+                            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                        }
+                    }
+                });
+    }
+
+    private void addUsertoDB(String email, String username, String fullname) {
+        User newuser = new User(email, fullname);
+
+        Log.d(TAG, "createuser");
+
+        DatabaseReference newUserRef = userDatabase.child(username);
+        newUserRef.setValue(newuser);
+    }
+
 }
