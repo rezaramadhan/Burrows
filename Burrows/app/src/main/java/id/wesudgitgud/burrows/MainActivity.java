ff40b48a47aa279ba2e26bd926cc75e555756ae2
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-02-23 20:47:50 +0700

    Add google services json, add button on mainactivity

--- Burrows/app/src/main/java/id/wesudgitgud/burrows/MainActivity.java
+++ Burrows/app/src/main/java/id/wesudgitgud/burrows/MainActivity.java
@@ -1,7 +1,9 @@
 package id.wesudgitgud.burrows;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoShop(View v) {
+        startActivity(new Intent(MainActivity.this, ShopActivity.class));
+    }
+
+    public void gotoItem(View v) {
+        startActivity(new Intent(MainActivity.this, ShopActivity.class));
     }
 }
