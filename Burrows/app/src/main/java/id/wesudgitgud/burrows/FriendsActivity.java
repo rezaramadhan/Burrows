6bd85ed00879813f4761445e69bf782b6b15447f
Mayu <mayuuzu@gmail.com>
2017-02-25 00:17:53 +0700

    membuat layout, membuat perpindahan activity/intent

--- Burrows/app/src/main/java/id/wesudgitgud/burrows/FriendsActivity.java
+++ Burrows/app/src/main/java/id/wesudgitgud/burrows/FriendsActivity.java
@@ -1,7 +1,10 @@
 package id.wesudgitgud.burrows;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FriendsActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class FriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        TextView titleMenu = (TextView)findViewById(R.id.textTitleMenu);
+        titleMenu.setText("Friends");
+    }
+
+    public void gotoMain(View v) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void gotoShop(View v) {
+        startActivity(new Intent(this, ShopActivity.class));
+    }
+
+    public void gotoItem(View v) {
+        startActivity(new Intent(this, ItemsActivity.class));
+    }
+
+    public void gotoAchievement(View v) {
+        startActivity(new Intent(this, AchievementActivity.class));
+    }
+
+    public void gotoFriend(View v) {
+        startActivity(new Intent(this, FriendsActivity.class));
     }
 }
--- Burrows/app/src/main/java/id/wesudgitgud/burrows/MainActivity.java
+++ Burrows/app/src/main/java/id/wesudgitgud/burrows/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView titleMenu = (TextView)findViewById(R.id.textTitleMenu);
+        titleMenu.setText("Burrows");
 
         viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
         next = (Button) findViewById(R.id.btnNext);
@@ -35,11 +39,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    public void gotoMain(View v) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
     public void gotoShop(View v) {
         startActivity(new Intent(MainActivity.this, ShopActivity.class));
     }
 
     public void gotoItem(View v) {
         startActivity(new Intent(MainActivity.this, ItemsActivity.class));
+    }
+
+    public void gotoAchievement(View v) {
+        startActivity(new Intent(MainActivity.this, AchievementActivity.class));
+    }
+
+    public void gotoFriend(View v) {
+        startActivity(new Intent(MainActivity.this, FriendsActivity.class));
     }
 }
